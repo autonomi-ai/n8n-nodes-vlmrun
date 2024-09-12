0ffd9a3b63b75fd493c2ebbef55a850cc6f10d85
Shahrear Bin Amin <shahrearbinamin1@gmail.com>
2024-09-12 18:35:45 +0600

    fix: add inital vlm node

--- nodes/VlmRun/VlmRun.node.ts
+++ nodes/VlmRun/VlmRun.node.ts
@@ -6,6 +6,7 @@ import {
 	NodeOperationError,
 } from 'n8n-workflow';
 import FormData from 'form-data';
+import { baseUrl } from './config';
 
 export class VlmRun implements INodeType {
 	description: INodeTypeDescription = {
@@ -14,7 +15,7 @@ export class VlmRun implements INodeType {
 		icon: 'file:vlm-run.svg',
 		group: ['transform'],
 		version: 1,
-		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		subtitle: '={{$parameter["operation"]',
 		description: 'Interact with VLM.run API',
 		defaults: {
 			name: 'VLM.run',
@@ -42,6 +43,10 @@ export class VlmRun implements INodeType {
 					{
 						name: 'Image AI',
 						value: 'imageAi',
+					},
+					{
+						name: 'File',
+						value: 'file',
 					},
 				],
 				default: 'docAi',
@@ -77,20 +82,78 @@ export class VlmRun implements INodeType {
 						name: 'AI-assisted Form Filling',
 						value: 'formFilling',
 					},
-					{
-						name: 'Multi-modal RAG',
-						value: 'multimodalRag',
-					},
 				],
 				default: 'resumeParser',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['imageAi'],
+					},
+				},
+				options: [
+					{
+						name: 'Image Cataloging',
+						value: 'imageCataloging',
+					},
+					{
+						name: 'Image Captioning',
+						value: 'imageCaptioning',
+					},
+				],
+				default: 'imageCataloging',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['file'],
+					},
+				},
+				options: [
+					{
+						name: 'Upload',
+						value: 'upload',
+					},
+					{
+						name: 'Delete',
+						value: 'delete',
+					},
+					{
+						name: 'List',
+						value: 'list',
+					},
+				],
+				default: 'upload',
 			},
 			{
 				displayName: 'File',
 				name: 'file',
 				type: 'string',
-				default: '',
+				default: 'data',
 				required: true,
-				description: 'The file to be processed (path to file)',
+				description: 'File data from previous node',
+			},
+			{
+				displayName: 'Model',
+				name: 'model',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'VLM-1',
+						value: 'vlm-1',
+					},
+				],
+				default: 'vlm-1',
+				description: 'The model to use for processing',
 			},
 		],
 	};
@@ -101,41 +164,57 @@ export class VlmRun implements INodeType {
 		const resource = this.getNodeParameter('resource', 0) as string;
 		const operation = this.getNodeParameter('operation', 0) as string;
 		const credentials = (await this.getCredentials('vlmRunApi')) as { apiKey: string };
+		const binaryFile = this.getNodeParameter('file', 0) as string;
+		const model = this.getNodeParameter('model', 0) as string;
 
 		for (let i = 0; i < items.length; i++) {
 			try {
 				if (resource === 'docAi') {
 					if (operation === 'resumeParser') {
 						// const filePath = this.getNodeParameter('file', i) as string;
+						const item = items[i];
+						if (!item.binary) {
+							throw new NodeOperationError(this.getNode(), 'No binary data exists on item!');
+						}
+
+						const binaryData = item.binary[binaryFile];
+						if (item.binary === undefined || binaryData === undefined) {
+							throw new NodeOperationError(
+								this.getNode(),
+								`No binary data property "${binaryFile}" does not exists on item!`,
+							);
+						}
 
 						// Step 1: Upload file
+						const buffer = await this.helpers.getBinaryDataBuffer(i, binaryFile);
+
 						const form = new FormData();
-						const fileContent = await this.helpers.binaryToBuffer(
-							await this.helpers.getBinaryDataBuffer(i, 'file'),
-						);
-						const fileName = this.getNodeParameter('file', i) as string;
-						form.append('file', fileContent, { filename: fileName });
+						// const fileContent = await this.helpers.binaryToBuffer(
+						// 	await this.helpers.getBinaryDataBuffer(i, 'file'),
+						// );
+						form.append('file', buffer, { filename: binaryData.fileName });
 
 						const uploadResponse = await this.helpers.httpRequest({
 							method: 'POST',
-							url: 'https://api.vlm.run/v1/files',
+							url: `${baseUrl}/files`,
 							headers: {
 								Authorization: `Bearer ${credentials.apiKey}`,
 							},
 							body: form,
 						});
 
-						const fileId = uploadResponse.file_id;
+						const fileId = uploadResponse.id;
 
 						// Step 2: Generate structured output
 						const generateResponse = await this.helpers.httpRequest({
 							method: 'POST',
-							url: 'https://api.vlm.run/v1/document/generate',
+							url: `${baseUrl}/document/generate`,
 							headers: {
 								Authorization: `Bearer ${credentials.apiKey}`,
 								'Content-Type': 'application/json',
 							},
 							body: JSON.stringify({
+								model: model,
 								file_id: fileId,
 							}),
 						});
---
+++ nodes/VlmRun/config.ts
@@ -0,0 +1,1 @@
+export const baseUrl = 'https://api.vlm.run/v1';
